370e09ab4d82f12d9d598b5cb7bb8ceaf134ff3c
해분 <62525605+HaeBun@users.noreply.github.com>
2025-03-25 11:14:09 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 888 ms, Memory: 116696 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Dot implements Comparable<Dot> {
+    int x;
+    int y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Dot dot) {
+        if(dot.y < this.y)
+            return 1;
+        else if(dot.y > this.y)
+            return -1;
+        else {
+            if(dot.x < this.x)
+                return 1;
+            else if(dot.x > this.x)
+                return -1;
+        }
+        return 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        Dot[] array = new Dot[n];
+
+        for(int i = 0; i < n; i++) {
+            String line = bufferedReader.readLine();
+            int x = Integer.parseInt(line.split(" ")[0]);
+            int y = Integer.parseInt(line.split(" ")[1]);
+
+            array[i] = new Dot(x, y);
+        }
+
+        Arrays.sort(array);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            stringBuilder.append(array[i].x +" " + array[i].y).append('\n');
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
