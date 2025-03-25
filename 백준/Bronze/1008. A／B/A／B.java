f29843e0ef866c340673f2fa227117ced2eeb518
해분 <62525605+HaeBun@users.noreply.github.com>
2025-03-25 10:59:48 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 15968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        double a = Sc.nextDouble();
+        double b = Sc.nextDouble();
+         
+        System.out.println(a / b);
+    }
+}
