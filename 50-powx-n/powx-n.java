c2d2209fdb95046080e06fc5275ac65ea57c59d7
해분 <62525605+HaeBun@users.noreply.github.com>
2025-04-07 15:20:58 +0900

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.34%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long longN = n;
+
+        if (n == 0)
+            return 1;
+        else if (n < 0) {
+            return powCalculator(1.0 / x, -longN, 1.0); // long 타입으로 넘겨줌, 1 나누기 제곱
+        } else
+            return powCalculator(x, longN, 1.0);
+    }
+
+    public double powCalculator(double base, long index, double result) {
+        if (index == 0)
+            return result;
+        else if (index % 2 == 1)
+            return powCalculator(base * base, index / 2, result * base);
+        else
+            return powCalculator(base * base, index / 2, result);
+    }
+}
